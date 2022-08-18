69490dbf174b96b63525e8ede4d309fa779af27a
DongHyun <ehdgus8054@gmail.com>
2022-08-18 17:51:16 +0900

    원본, 번역본 둘다 보이게 수정

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# NLP_Project_3
-자연어처리 세 번째 프로젝트 협업 공간입니다.
+# 실행 환경
+* 구글 코랩 Pro+ 에서 테스트했습니다.
+
+# 설치 패키지
+* !pip install flask_ngrok
+* !pip install pyngrok==4.1.1
+* !pip install transformers
+* !pip install sentencepiece
+* !pip install torch
+* !pip install kss
+* !pip install nltk
+* !pip install bert_score
+
+# ngrok에서 가입후 토큰을 받아온다.
+* 주소 : https://ngrok.com/
+* !ngrok authtoken "토큰" # 쌍따옴표를 남긴다.
+
+# 학습모델은 팀 프로젝트 드라이브에 올림
+
+# Usage
+* server.py 코드를 복사 후 코랩에 붙여넣기 해서 실행한다.
---
+++ text_summarization.py
@@ -0,0 +1,95 @@
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+import re
+from sklearn.metrics.pairwise import cosine_similarity
+import networkx as nx
+import numpy as np
+import os
+from urllib.request import urlretrieve
+import zipfile
+
+glove_dict = dict()
+
+  
+nltk.download('stopwords')
+nltk.download('punkt')
+
+if not(os.path.isfile('glove.6B.zip')):
+    urlretrieve("http://nlp.stanford.edu/data/glove.6B.zip", filename="glove.6B.zip")
+    zf = zipfile.ZipFile('glove.6B.zip')
+    zf.extractall() 
+    zf.close()
+
+with open('glove.6B.100d.txt', encoding="utf8") as f: # 100차원의 GloVe 벡터를 사용 
+    for line in f:
+        word_vector = line.split()
+        word = word_vector[0]
+        word_vector_arr = np.asarray(word_vector[1:], dtype='float32') # 100개의 값을 가지는 array로 변환
+        glove_dict[word] = word_vector_arr
+
+def summarize(text):
+  # 토큰화 함수
+  def tokenization(sentences):
+      return [word_tokenize(sentence) for sentence in sentences]
+
+  # 전처리 함수
+  def preprocess_sentence(sentence):
+    # 영어를 제외한 숫자, 특수 문자 등은 전부 제거. 모든 알파벳은 소문자화
+    sentence = [re.sub(r'[^a-zA-z\s]', '', word).lower() for word in sentence]
+    # 불용어가 아니면서 단어가 실제로 존재해야 한다.
+    return [word for word in sentence if word not in stop_words and word]
+
+  # 위 전처리 함수를 모든 문장에 대해서 수행. 이 함수를 호출하면 모든 행에 대해서 수행.
+  def preprocess_sentences(sentences):
+      return [preprocess_sentence(sentence) for sentence in sentences]
+
+  # 단어 벡터의 평균으로부터 문장 벡터를 얻는다.
+  def calculate_sentence_vector(sentence):
+    if len(sentence) != 0:
+      return sum([glove_dict.get(word, zero_vector) 
+                    for word in sentence])/len(sentence)
+    else:
+      return zero_vector
+
+  # 각 문장에 대해서 문장 벡터를 반환
+  def sentences_to_vectors(sentences):
+      return [calculate_sentence_vector(sentence) 
+                for sentence in sentences]
+
+  def similarity_matrix(sentence_embedding):
+    sim_mat = np.zeros([len(sentence_embedding), len(sentence_embedding)])
+    for i in range(len(sentence_embedding)):
+        for j in range(len(sentence_embedding)):
+          sim_mat[i][j] = cosine_similarity(sentence_embedding[i].reshape(1, embedding_dim),
+                                            sentence_embedding[j].reshape(1, embedding_dim))[0,0]
+    return sim_mat
+
+  def calculate_score(sim_matrix):
+      nx_graph = nx.from_numpy_array(sim_matrix)
+      scores = nx.pagerank(nx_graph, max_iter = 100000, tol=1.0e-3)  #오류때문에 max_iter 추가
+      return scores
+
+  def ranked_sentences(sentences, scores, n=5):   # 요약 5 문장으로 출력
+      top_scores = sorted(((scores[i], s, order) for order, (i,s) in enumerate(enumerate(sentences))), key=lambda x: x[0], reverse=True)
+      top_n_sentences = [sentence for score, sentence, order in sorted(top_scores[:n], key=lambda x: x[2])]
+      return " ".join(top_n_sentences)
+
+  # glove_dict = init.get_glove_dict()
+  stop_words = stopwords.words('english')
+  
+  data = {}
+  data['content'] = text
+  data['sentences'] = sent_tokenize(data['content'])
+  data['tokenized_sentences'] = tokenization(data['sentences'])
+  data['tokenized_sentences'] = preprocess_sentences(data['tokenized_sentences'])
+  embedding_dim = 100
+  zero_vector = np.zeros(embedding_dim)
+  data['SentenceEmbedding'] = sentences_to_vectors(data['tokenized_sentences'])
+  data['SimMatrix'] = similarity_matrix(data['SentenceEmbedding'])
+  data['score'] = calculate_score(data['SimMatrix'])
+  data['summary'] = ranked_sentences(data['sentences'], data['score'])
+  result = data['summary']
+  del data
+
+  return result
